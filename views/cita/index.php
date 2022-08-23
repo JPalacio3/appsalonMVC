37865f7e017a2a7dade473ecfcce74e1d7b31820
JPalacio <Joelpalacio630@gmail.com>
2022-08-23 18:08:04 -0500

    cpmmit

--- views/cita/index.php
+++ views/cita/index.php
@@ -50,6 +50,17 @@
         <p class="text-center">Verifica que la información sea correcta</p>
     </div>
 
+    <div class="paginacion">
+        <button id="anterior" class="boton">
+            &laquo; Anterior
+        </button>
+
+        <button id="siguiente" class="boton">
+            Siguiente &raquo;
+        </button>
+
+    </div>
+
 
 
 
