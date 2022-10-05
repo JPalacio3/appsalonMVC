66c055721fc17f9c4865c01857cefd131e818de0
JPalacio <Joelpalacio630@gmail.com>
2022-10-05 17:20:33 -0500

    añadiendo el filtro de calendario

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -11,6 +11,8 @@ class AdminController
     {
         session_start();
 
+        $fecha = date('Y-m-d');
+
         // Consultar la base de datos
         $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
         $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
@@ -21,7 +23,7 @@ class AdminController
         $consulta .= " ON citasServicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
         $consulta .= " ON servicios.id=citasServicios.servicioId ";
-        // $consulta .= " WHERE fecha =  '${fecha}' ";
+        $consulta .= " WHERE fecha =  '${fecha}' ";
 
         $citas = AdminCita::SQL($consulta);
 
@@ -29,7 +31,8 @@ class AdminController
 
         $router->render('admin/index', [
             'nombre' => $_SESSION['nombre'],
-            'citas' => $citas
+            'citas' => $citas,
+            'fecha' => $fecha
         ]);
     }
 }
--- views/admin/index.php
+++ views/admin/index.php
@@ -9,7 +9,7 @@
     <form action="" class="formulario">
         <div class="campo">
             <label for="fecha">Fecha: </label>
-            <input type="date" id="fecha" name="fecha" />
+            <input type="date" id="fecha" name="fecha" value="<?php echo $fecha; ?>" />
         </div>
     </form>
 </div>
